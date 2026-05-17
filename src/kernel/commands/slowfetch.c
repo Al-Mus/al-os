8a8e8098fef56d3801098f11ed5a7952918d5597
Al-Mus <musalerus@gmail.com>
2026-05-18 00:16:06 +0400

    Починил команду slowfetch.c

--- src/kernel/commands/slowfetch.c
+++ src/kernel/commands/slowfetch.c
@@ -1,39 +1,49 @@
-#include "../drivers/vga/vga.h"
 #include "all_commands.h"
+#include "../drivers/vga/vga.h"
 #include "../drivers/keyboard/keyboard.h"
 #include "../drivers/vga/colors.h"
+#include "../arch/i686/timer/timer.h"
 #include "../sys/metadata.h"
 
 
-
-static void slowprint_line(const char* str, uint8_t color, unsigned int delay) {
+static void slowprint_line(const char* str, uint8_t color, uint32_t delay_ms) {
     uint8_t old = vga_color;
     vga_color = color;
+
     for (int i = 0; str[i]; i++) {
+
+        keyboard_poll();
+        if (keyboard_sigint_check()) {
+            vga_print_color("\nOperation cancelled\n", LIGHT_RED);
+            vga_color = old;
+            return;
+        }
+
         vga_putc(str[i]);
-        for (volatile unsigned int j = 0; j < delay; j++) {
-            if ((j & 0xFFF) == 0) {
-                keyboard_poll();
-                if (keyboard_sigint_check()) {
-                    vga_print_color("\nOperation cancelled\n", LIGHT_RED);
-                    vga_color = old;
-                    return;
-                }
-            }
+
+        if (delay_ms) {
+            sleep(delay_ms);
         }
     }
+
     vga_putc('\n');
     vga_color = old;
 }
 
 static void slowprint_logo() {
     for (int i = 0; AL_OS_LOGO[i]; i++) {
-        slowprint_line(AL_OS_LOGO[i], 0x0B, 50000);
+        slowprint_line(AL_OS_LOGO[i], 0x0B, 20);
     }
 }
 
+static void slowprint_release_note() {
+        slowprint_line(RELEASE_NOTES, 0x0B, 20);
+}
+
 void cmd_slowfetch(void) {
-    slowprint_line("", 0x0B, 50000);
+    slowprint_line("", 0x0B, 20);
     slowprint_logo();
-    slowprint_line("", 0x07, 50000);
+    slowprint_line("", 0x07, 20);
+    slowprint_release_note();
+    slowprint_line("", 0x07, 20);
 }
